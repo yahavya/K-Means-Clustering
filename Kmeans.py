b1b9fd530ae0be8b876f8d77f93209c1c1aa4fef
Ron <ronnieyahav@gmail.com>
2024-06-30 11:34:58 +0300

    add Kmeans file for python with description

---
+++ Kmeans.py
@@ -0,0 +1,8 @@
+"""
+Given a set of N datapoints x1,x2,...,xN ∈ Rd, 
+the goal is to group the data into K ∈ N clusters, 
+each datapoint is assigned to exactly one cluster 
+and the number of clusters K is such that 1 < K < N . 
+Each cluster k is represented by its centroid, 
+which is the mean μk ∈ Rd of the cluster’s members.
+"""
